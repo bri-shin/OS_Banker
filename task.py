7b3ea427d27304cade1471e630a1b3c19c3d4ed0
Brian Shin <bsshinple@gmail.com>
2019-11-06 12:34:16 -0500

    added FIFO allocation; started Banker

--- task.py
+++ task.py
@@ -5,45 +5,51 @@ Class Activity is a representation of an activity that a
 task should complete
 '''
 
+
 class Activity():
     def __init__(self, *args):
-        self.status = args[0]               # Activity Status: initiate, request, compute, relase, and terminate
-        
+        # Activity Status: initiate, request, compute, relase, and terminate
+        self.status = args[0]
+
         # Initate
         if self.status == 'initiate':
             self.taskNum_ = args[1]
             self.resourceType = args[2]
             self.claim = args[3]
-        
+
         # Request
         elif self.status == 'request':
             self.taskNum_ = args[1]
             self.resourceType = args[2]
             self.request = args[3]
-        
+
         # Release
         elif self.status == 'release':
             self.taskNum_ = args[1]
             self.resourceType = args[2]
             self.release = args[3]
-        
+
         # Computing
         elif self.status == 'compute':
             self.taskNum_ = args[1]
             self.cycleSize = args[2]
-        
+
         # Terminate
         elif self.status == 'terminate':
             self.taskNum_ = args[1]
+
 
 ''' 
 Class Task is a representation of a task, consisting 
 of information about the task specifics and activities
 '''
+
+
 class Task():
-    def __init__(self,*args):
+    def __init__(self, *args):
         # > Task Attributes
-        self.state = "unstarted"            # Task can be: unstarted, running, blocked, terminated, or aborted
+        # Task can be: unstarted, running, blocked, terminated, or aborted
+        self.state = "unstarted"
         self.taskNum = args[0]              # Task Number
         self.initialClaim = [0] * args[-1]  # Initial claim of resource
         self.holding = [0] * args[-1]       # Resources held by task
@@ -54,7 +60,8 @@ class Task():
         self.waitTime = 0                   # Time / Cycles a task waited
         self.blockedCycle = -1              # Time / Cycle when task was blocked
 
-        self.execution = []                 # List of activities to execute (in order)
+        # List of activities to execute (in order)
+        self.execution = []
 
     ''' > Functions that Change Task State'''
 
@@ -67,18 +74,19 @@ class Task():
         self.state = "running"
         self.blockedCycle = -1
         self.currentCycle += 1
-    
+
     # Settin Task state to blocked (it not deadlocked) and adds rejected activities into execution list
-    def block(self, activity = None, deadlocked = False):
+    def block(self, activity=None, deadlocked=False):
         if activity is not None:
-            self.execution.inset(0, activity)       # need to be constnatly in front of execution list
+            # need to be constnatly in front of execution list
+            self.execution.inset(0, activity)
         if deadlocked == False:
             self.state = "blocked"
             if self.blockedCycle < 0:
                 self.blockedCycle = self.currentCycle
-            self.waitTime +=1
+            self.waitTime += 1
             self.currentCycle += 1
-    
+
     # Setting Task State to terminated
     def terminate(self):
         self.state = "terminated"
@@ -106,7 +114,7 @@ class Task():
     # Getting Task's Claim
     def getClaim(self, resourceType):
         return self.claim[resourceType-1]
-    
+
     # Changing Claim Value
     def changeClaim(self, resourceType, value):
         self.claim[resourceType-1] = value
@@ -119,20 +127,29 @@ class Task():
         self.currentCycle += 1
 
     # Getting held resources and updating value of held resource
-    def updateHoldResource(self, resourceType, value = -1):
+    def updateHoldResource(self, resourceType, value=-1):
         if value >= 0 and value <= self.getClaim(resourceType):
-            self.holding[resourceType -1] = value
-        return self.holding[resourceType -1]
+            self.holding[resourceType - 1] = value
+        return self.holding[resourceType - 1]
+
+    ''' > Checker Function'''
+
+    # Checking whether task is computing
+    def bool_compute(self):
+        return self.remainingCycle > 0
+
 
 '''
 Class TaskList is the list of all tasks that needs to be exectued
 using resource allocation algorithms (FIFO / Banker)
 '''
 
+
 class TaskList(list):
     def __inint__(self, allocType):
         super(TaskList, self).__init__()
-        self.resource = 0                       # Total number of resource Type (careful of name)
+        # Total number of resource Type (careful of name)
+        self.resource = 0
         self.currentResource = []                   # List of current resources
         self.totalResource = []                     # List of total amount of resources
 
@@ -140,12 +157,13 @@ class TaskList(list):
         if allocType == "FIFO":
             self.allocType = FIFO()                 # Running FIFO by creating FIFO instance
         elif allocType == "Banker":
-            self.allocType = Banker()               # Running Banker by creating Banker instance
-        
+            # Running Banker by creating Banker instance
+            self.allocType = Banker()
+
         # Starting Resource Allocation
         def start(self):
             self.allocType.run(self)
-        
+
         ''' > Functions Managing Activity'''
 
         # New activities added depending on input parameter (task number)
@@ -155,11 +173,11 @@ class TaskList(list):
             task.newActivity(activity)
 
         ''' > Functions Managing Tasks'''
-        
+
         # New tasks created with input parameter & appended to taskList
         def newTask(self, *specs):
             self.append(Task(*specs, self.resource))
-        
+
         # Retrieving tasks in particular state
         def taskByState(self, tState):
             tasks = []
@@ -170,36 +188,37 @@ class TaskList(list):
 
         ''' > Functions Managing Resources'''
         # Getting held resources and updating value of held resource
-        def updateHoldResource(self, resourceType, value = -1):
+
+        def updateHoldResource(self, resourceType, value=-1):
             if value >= 0:
-                self.holding[resourceType -1] = value
-            return self.holding[resourceType -1]
+                self.holding[resourceType - 1] = value
+            return self.holding[resourceType - 1]
 
         # Responds to task request through resource allocation
         def allocate(self, task, resourceType, value):
             # Updating Task Resource
             taskResource = task.updateHoldResource(resourceType)
-            task.updateHoldResource(resourceType, taskResource+ value)
-            
+            task.updateHoldResource(resourceType, taskResource + value)
+
             # Update TaskList
             heldResource = self.updateHoldResource(resourceType)
             self.updateHoldResource(resourceType, heldResource - value)
-        
+
         # Release resource when told to release
-        def release(self, task, resourceType = None, value = None):
+        def release(self, task, resourceType=None, value=None):
             if resourceType is not None and value is not None:
                 # Updating Task Resource
                 taskResource = task.updateHoldResource(resourceType)
                 task.updateHoldResource(resourceType, taskResource - value)
-                
+
                 # Update TaskList
                 heldResource = self.updateHoldResource(resourceType)
                 self.updateHoldResource(resourceType, heldResource + value)
             else:
                 # To release all of the resource
                 for i in range(1, 1+self.resouce):
-                    self.updateHoldResource(i, self.updateHoldResource(i) + task.updateHoldResource(i))
-
+                    self.updateHoldResource(i, self.updateHoldResource(
+                        i) + task.updateHoldResource(i))
 
         ''' > Checker Functions'''
 
@@ -220,14 +239,14 @@ class TaskList(list):
                 return True
 
         # Boolean that checks whether state is safe (Banker's Algo)
-        # Safe means all requests can be satisfied 
+        # Safe means all requests can be satisfied
         def bool_safe(self, task):
-            for i in range(1,self.resourceType+1):
+            for i in range(1, self.resourceType+1):
                 if task.getClaim(i) - task.updateHoldResource(i) > self.updateHoldResource(i):
                     return False
             return True
-            
-        # Boolean that checks for deadlocks 
+
+        # Boolean that checks for deadlocks
         # Deadlock means that unfinished tasks are blocked due to allocation problems (no requests can be further satisfied)
         def bool_deadlock(self):
             running = self.taskByState('running')
@@ -236,13 +255,146 @@ class TaskList(list):
                 return False
             for bt in blocked:
                 activity = bt.getActivity()
-                bt.block(activity, deadlocked= True)
+                bt.block(activity, deadlocked=True)
                 if self.bool_allocate(activity.resourceType, activity.claim):
                     return False
-            
+
             return True
 
+        ''' > Utility Function'''
 
-# class FIFO():
-    
-# class Banker():
+        def printResult(self):
+            print(self.allocType)
+            cycleSum = 0
+            waitSum = 0
+            for task in self:
+                if task.state == "aborted":
+                    print("Task {}:\t{}".format(task.taskNum, task.state))
+                else:
+                    print("Task {}:\t{}\t{}\t{}%".format(
+                        task.taskNum, task.currentCycle, task.waitTime, task.waitTime/task.currentCycle * 100))
+                    cycleSum += task.currentCycle
+                    waitSum += task.waitTime
+            print("Total:\t{}\t{}\t{}%".format(
+                cycleSum, waitSum, waitSum/cycleSum * 100))
+
+
+'''
+Class FIFO is a simulation of the optimistic resource manager that processes tasks in FIFO
+'''
+
+
+class FIFO():
+    def __init__(self, *args):
+        self.argument = args
+        self.type = "FIFO"
+
+    def start(self, taskManager):
+        # Once all tasks are finished (terminated), the result is printed
+        if taskManager.bool_terminated():
+            taskManager.printResult()
+            return
+
+        # Step 1: Check blocked tasks to determine whether allocation is possible
+        blocked = taskManager.taskByState("blocked")
+        blocked.sort(key=lambda task: task.blockedCycle)
+        for i in blocked:
+            activity = i.getActivity()
+            if taskManager.bool_allocate(activity.resourceType, activity.claim):
+                taskManager.allocate(i, activity.resourceType, activity.claim)
+                i.unstart()
+            else:
+                i.block(activity)
+
+        # Step 2: Check running tasks. If computing, let it compute. If not, process
+        running = taskManager.taskByState("running")
+        releasedResource = []
+        for i in running:
+            if i.bool_compute():
+                i.updateComputeTime()
+                continue
+            activity = i.getActiviy()
+            # request compute release terminate
+
+            if activity.status == "request":
+                # Check whether the resource can be allocated.
+                if taskManager.bool_allocate(activity.resourceType, activity.request):
+                    taskManager.allocate(
+                        i, activity.resourceType, activity.request)
+                    i.run()
+                else:
+                    i.block(activity)
+            # When Activity status is released, release
+            elif activity.status == "release":
+                releasedResource.append(
+                    [i, activity.resourceType, activity.relase])
+                i.run()
+            # When Activity status is compute, update compute time
+            elif activity.status == "compute":
+                i.updateComputeTime(activity.cycleSize)
+            # When Activity status is terminate, terminate
+            elif activity.status == "terminate":
+                i.terminate()
+        for released in releasedResource:
+            taskManager.release(*released)
+
+        # Step 3: Starting unstarted tasks
+        unstarted = taskManager.taskByState("unstarted")
+        for i in unstarted:
+            if i.nextAcitivty() == "initiate":
+                activity = i.getActivity()
+                i.changeClaim(activity.resourceType, activity.claim)
+            i.run()
+            
+            # If next activity is still intiate, unstart
+            if i.nextActivity() == "initiate":
+                i.unstart()
+        
+        # Dealing with deadlocks
+        while taskManager.bool_deadlock():
+            # while deadlocked, aborting tasks
+            abortTask = taskManager.taskByState("blocked")[0]
+            abortTask.abort()
+            taskManager.release(abortTask)
+
+        self.start(taskManager)
+
+'''
+Class Banker is a simulation of Banker Algorithm (resource manager)
+'''
+class Banker():
+    def __init__(self, *args):
+        self.argument = args
+        self.type = "FIFO"
+
+    def start(self, taskManager):
+        # Once all tasks are finished (terminated), the result is printed
+        if taskManager.bool_terminated():
+            taskManager.printResult()
+            return
+        # Step 1: Check blocked tasks to determine whether allocation is safe
+        blocked = taskManager.taskByState("blocked")
+        blocked.sort(key=lambda task: task.blockedCycle)
+        for i in blocked:
+            activity = i.getActivity()
+            # Check safety before allocation
+            if taskManager.bool_safe(i):
+                taskManager.allocate(i, activity.resourceType,activity.request)
+                i.unstart()
+            # If unsafe, block
+            else:
+                i.block(activity)
+
+        # Step 2: Check running tasks. If computing, let it compute. If not, process
+        running = taskManager.taskByState("running")
+        releasedResource = []
+        for i in running:
+            if i.bool_compute():
+                i.updateComputeTime()
+                continue
+            activity = i.getActiviy()
+            
+            # request compute release terminate
+            if activity.status == "request":
+                if i.get
+
